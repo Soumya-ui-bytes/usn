2f87f0607dd81c88c4c714fadcddee056ad7cdaa
Soumya-ui-bytes <soumyarajagopal027@gmail.com>
2025-09-30 09:37:22 +0530

    main branch

---
+++ usn.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+usn = input("Enter your USN: ")
+course = input("Enter your course: ")
+
+print("\nYour Details:")
+print(f"Name: {name}")
+print(f"USN: {usn}")
+print(f"Course: {course}")
